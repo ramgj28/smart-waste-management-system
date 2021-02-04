bdfd9d0925285de3a2b88852eedcdab625b347f4
Ramgopal J <ramgopaljayram@gmail.com>
2021-02-04 13:00:07 +0530

    comments added

--- wasteservo.ino
+++ wasteservo.ino
@@ -1,20 +1,20 @@
 #include <ESP8266WiFi.h>  
 #include <Servo.h>
-const int IR       = D3;   
-const int trigPin1 = D5;   
-const int echoPin1 = D6;  
-const int trigPin2 = D7;   
-const int echoPin2 = D8;  
+const int IR       = D3;                           // output from IR sensor  
+const int trigPin1 = D5;                           // Trigger to Ultrasonic sensor in bin 1
+const int echoPin1 = D6;                           // Echo from Ultrasonic sensor in bin 1 
+const int trigPin2 = D7;                           // Trigger to Ultrasonic sensor in bin 2
+const int echoPin2 = D8;                           // Echo from Ultrasonic sensor in bin 2 
 Servo myservo; 
 long duration1,duration2; 
 int distance1,distance2;  
 
 WiFiClient client;
 WiFiServer server(80);  
-const char* MY_SSID = "RGJ2000"; 
-const char* MY_PWD = "ramg1245";
+const char* MY_SSID = "MY_SSID";                   // WIFI network ID
+const char* MY_PWD = "MY_PWD";                     // password of your wifi network
 const char* TS_SERVER = "api.thingspeak.com"; 
-String TS_API_KEY ="1DU031WUVU2DH79X";
+String TS_API_KEY ="XXXXXXXXXXXXXXXX";             // API key of your channel from thingspeak
 
 void connectWifi() 
 { 
@@ -38,10 +38,10 @@ void connectWifi()
 
 void sendDataTS(void)
 {   
-  int water_value = analogRead(A0);
+  int water_value = analogRead(A0);                    //reading value from moisture sensor
   Serial.print("Water_Value:  ");   
   Serial.println(water_value); 
-  if(digitalRead(IR)==LOW && water_value>=1024)//moisture cutoff to be added
+  if(digitalRead(IR)==LOW && water_value>=1024)        //checking values of moisture and IR sensors. Condition for dry waste.
   {
     myservo.write(180);
     Serial.println(" dry ");
@@ -49,7 +49,7 @@ void sendDataTS(void)
     myservo.write(90);
     delay(2000);   
   }
-  else if(digitalRead(IR)==LOW && water_value<1024)//moisture cutoff to be added
+  else if(digitalRead(IR)==LOW && water_value<1024)    //condition for wet waste.
   {
    myservo.write(0);
    Serial.println(" wet ");
@@ -63,8 +63,8 @@ void sendDataTS(void)
   delayMicroseconds(10);  
   digitalWrite(trigPin1, LOW);   
   duration1 = pulseIn(echoPin1, HIGH);
-  distance1 = duration1*0.034/2;  
-  Serial.print("Distance_1: ");    
+  distance1 = duration1*0.034/2;                      // indicator of space left in bin 1
+  Serial.print("Distance_1: ");       
   Serial.println(distance1);    
   delay(1000); 
   digitalWrite(trigPin2, LOW);  
@@ -74,7 +74,7 @@ void sendDataTS(void)
   digitalWrite(trigPin2, LOW);   
   duration2 = pulseIn(echoPin2, HIGH);  
   distance2 = duration2*0.034/2; 
-  Serial.print("Distance_2: "); 
+  Serial.print("Distance_2: ");                       // indicator of space left in bin 1
   Serial.println(distance2);    
   Serial.print("Water_Value:  ");   
   Serial.println(water_value); 
@@ -105,16 +105,16 @@ client.stop();
 void setup() 
 {
   Serial.begin(9600); 
-  myservo.attach(D1);
+  myservo.attach(D1);                                   //attaches servopin to D1 of nodemcu
   myservo.write(90);
   delay(100);   
   connectWifi(); 
   myservo.attach(D1);
-  pinMode(trigPin1, OUTPUT); // Sets the trigPin as an Output   
-  pinMode(echoPin1, INPUT); // Sets the echoPin as an Input  
-  pinMode(trigPin2, OUTPUT); // Sets the trigPin as an Output   
-  pinMode(echoPin2, INPUT); // Sets the echoPin as an Input  
-  pinMode(IR,INPUT); 
+  pinMode(trigPin1, OUTPUT);                           // Sets the trigPin as Output   
+  pinMode(echoPin1, INPUT);                            // Sets the echoPin as Input  
+  pinMode(trigPin2, OUTPUT);                           // Sets the trigPin as Output   
+  pinMode(echoPin2, INPUT);                            // Sets the echoPin as Input  
+  pinMode(IR,INPUT);                                   // Sets the IR pin as Output
 }  
  
 void loop()  
